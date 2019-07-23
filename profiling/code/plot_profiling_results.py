d0c4f33d6636b501ccad165ef3d71de8d1817414
stellalessandra <a.stella@fz-juelich.de>
2019-07-23 15:03:06 +0200

    fixed upper plot ylimits

--- profiling/code/plot_profiling_results.py
+++ profiling/code/plot_profiling_results.py
@@ -10,32 +10,13 @@ import os
 import seaborn as sns
 import yaml
 import quantities as pq
+from utils import mkdirp, split_path, estimate_number_spikes
 sns.set_style('ticks', {'xtick.direction': u'in', 'ytick.direction': u'in'})
 sns.set_context("paper")
 
-# Function to create new folders
-def mkdirp(directory):
-    if not os.path.isdir(directory):
-        os.mkdir(directory)
-
-# Function to split path to single folders
-def split_path(path):
-    folders = []
-    while 1:
-        path, folder = os.path.split(path)
-        if folder != "":
-            folders.append(folder)
-        else:
-            if path != "":
-                folders.append(path)
-            break
-    folders.reverse()
-    return folders
-
-
 # Script variables
-time_fpgrowth = []
-time_fast_fca = []
+time_fpgrowth = {}
+time_fast_fca = {}
 
 savefig = True
 
@@ -47,20 +28,28 @@ binsize = config['binsize'] * pq.ms
 rates = config['rate']
 t_stops = config['t_stop']
 ns = config['n']
+keys = config['keys']
 
-expected_num_spikes_dict = {}
-for n in ns:
-    for rate in rates:
-        for t_stop in t_stops:
-            expected_num_spikes_dict[n*rate*t_stop] = {
-                'n':n, 't_stop':t_stop, 'rate':rate}
-count_spikes = sorted(list(expected_num_spikes_dict.keys()))
+# check if the input is coherent
+if not set(keys).issubset({'rate', 'neurons', 'time'}):
+    raise ValueError('set of keys not valid')
+
+expected_num_spikes_dict = estimate_number_spikes(keys=keys,
+                                                  ns=ns,
+                                                  rates=rates,
+                                                  t_stops=t_stops)
+
 # Loading routine
-for num_spikes in count_spikes:
-    res = np.load(
-        "../results/{}/profiling_results.npy".format(num_spikes)).item()
-    time_fpgrowth.append(res['results']['fp_growth'])
-    time_fast_fca.append(res['results']['fast_fca'])
+for key in keys:
+    time_fast_fca[key] = []
+    time_fpgrowth[key] = []
+    # here infer the number of spikes for each key
+    count_spikes = list(expected_num_spikes_dict[key].keys())
+    for num_spikes in count_spikes:
+        res = np.load(
+            "../results/{}/{}/profiling_results.npy".format(key, num_spikes)).item()
+        time_fpgrowth[key].append(res['results']['fp_growth'])
+        time_fast_fca[key].append(res['results']['fast_fca'])
 
 
 def square(x, b, b0):
@@ -181,7 +170,7 @@ def compute_xy(data, xaxis, ax=None, **kwargs):
         interpolate : bool
             Determines if the curve should be interpolated or not
         linestyle : string
-            Set the linestyle of the plot
+            Set the linestyle of the key
 
     Returns
     -------
@@ -225,88 +214,128 @@ for i in range(1, NUM_COLORS + 1):
     color = cm(1. * i // NUM_COLORS)
     colores[i - 1] = color
 colores = cmap
-f, ax = plt.subplots(2, 1, figsize=(
-    8.5 / inch2cm, 11 / inch2cm))
-f.subplots_adjust(hspace=0.05, bottom=0.2)
+f, ax = plt.subplots(2, 3, figsize=(
+    18.4 / inch2cm, 9.2 / inch2cm))
+f.subplots_adjust(wspace=0.15, hspace=0.05, bottom=0.2, left=0.06, right=0.98)
 
 interpolate = False
 linestyle = ':'
 
-# Plotting all functions
-for idx, axes in enumerate(ax):
-    # Plot FP-growth
-    compute_xy(
-        time_fpgrowth, count_spikes, axes, function=square,
-        label="FP-growth (C++)", colors=colores[1], marker="o",
-        interpolate=interpolate, linestyle='-', markerfacecolor='None')
-    # Plot FCA
-    compute_xy(
-        time_fast_fca, count_spikes, axes, function=linear,
-        label="Fast-FCA (Python)", colors=colores[0], marker="o",
-        interpolate=interpolate, linestyle=linestyle,
-        markerfacecolor=colores[0])
-    # Plot Spectra
-    compute_xy(
-        np.array(time_fpgrowth) * 2000, count_spikes, axes, function=poly4,
-        label="2d FP-growth", colors=colores[9], marker="o",
-        interpolate=interpolate, linestyle='-', markerfacecolor='None')
-    compute_xy(
-        np.array(time_fpgrowth) * 2000,
-        count_spikes, axes, function=poly4,
-        label="3d FP-growth", colors=colores[3], marker="o",
-        interpolate=interpolate, linestyle=linestyle,
-        markerfacecolor=colores[3])
-    compute_xy(
-        np.array(time_fast_fca)*2000, count_spikes, axes, function=poly4,
-        label="2d Fast-FCA", colors=colores[4], marker="o",
-        interpolate=interpolate, linestyle='-',
-        markerfacecolor='None')
-    compute_xy(
-        np.array(time_fast_fca) * 2000,
-        count_spikes, axes, function=poly4,
-        label="3d Fast-FCA", colors=colores[2], marker="o",
-        interpolate=interpolate, linestyle=linestyle,
-        markerfacecolor=colores[2])
+y1_lower_plot = np.array([])
+y2_lower_plot = np.array([])
 
+for key_idx, key in enumerate(keys):
+    # Plotting all functions
+    # Create plots by column
+    axes_vertical = [ax[0][key_idx]] + [ax[1][key_idx]]
+    for idx, axes in enumerate(axes_vertical):
+        # Plot FP-growth
+        compute_xy(
+            time_fpgrowth[key], count_spikes, axes, function=square,
+            label="FP-growth (C++)", colors=colores[1], marker="o",
+            interpolate=interpolate, linestyle='-', markerfacecolor='None')
+        # Plot FCA
+        compute_xy(
+            time_fast_fca[key], count_spikes, axes, function=linear,
+            label="Fast-FCA (Python)", colors=colores[0], marker="o",
+            interpolate=interpolate, linestyle=linestyle,
+            markerfacecolor=colores[0])
+        # Plot Spectra
+        compute_xy(
+            np.array(time_fpgrowth[key]) * 2000, count_spikes, axes, function=poly4,
+            label="2d FP-growth", colors=colores[9], marker="o",
+            interpolate=interpolate, linestyle='-', markerfacecolor='None')
+        compute_xy(
+            np.array(time_fpgrowth[key]) * 2000,
+            count_spikes, axes, function=poly4,
+            label="3d FP-growth", colors=colores[3], marker="o",
+            interpolate=interpolate, linestyle=linestyle,
+            markerfacecolor=colores[3])
+        compute_xy(
+            np.array(time_fast_fca[key]) * 2000, count_spikes, axes, function=poly4,
+            label="2d Fast-FCA", colors=colores[4], marker="o",
+            interpolate=interpolate, linestyle='-',
+            markerfacecolor='None')
+        compute_xy(
+            np.array(time_fast_fca[key]) * 2000,
+            count_spikes, axes, function=poly4,
+            label="3d Fast-FCA", colors=colores[2], marker="o",
+            interpolate=interpolate, linestyle=linestyle,
+            markerfacecolor=colores[2])
 
-# Axes specific things
-# Ax 0
-ax[0].set_ylim(-10, np.max(np.array(time_fast_fca) * 2000) / 60.0 + 100)
-ax[0].set_ylabel("compute time (min)", size=label_size)
-ax[0].set_xticks(count_spikes)
-ax[0].set_xticklabels([], size=tick_size)
-# ax[0].set_xlabel("number of spikes", size=label_size)
-ax[0].tick_params(axis='both', length=2., labelsize=tick_size)
-# Ax 1
-ax[1].tick_params(axis='y', which='minor', left='off')
-ax[1].set_xticks(count_spikes)
-ax[1].set_xticklabels(count_spikes, size=tick_size)
-ax[1].set_xlabel("$N_s$", size=label_size)
-ax[1].set_ylabel('log (compute time)', size=label_size)
-ax[1].set_yscale('log')
-ax[1].tick_params(axis='both', length=2., labelsize=tick_size)
+    # Axes specific things
+    # Ax 0
 
-# Set second x-axis
-ax2 = ax[1].twiny()
+    ax[0][key_idx].set_xticks(count_spikes)
+    ax[0][key_idx].set_xticklabels([], size=tick_size)
+    # ax[0].set_xlabel("number of spikes", size=label_size)
+    ax[0][key_idx].tick_params(axis='both', length=2., labelsize=tick_size)
+    # Ax 1
+    ax[1][key_idx].tick_params(axis='y', which='minor', left='off')
+    ax[1][key_idx].set_xticks(count_spikes)
+    ax[1][key_idx].set_xticklabels(count_spikes, size=tick_size)
+    ax[1][key_idx].set_xlabel("$N_s$", size=label_size)
+    ax[1][key_idx].set_yscale('log')
+    ax[1][key_idx].tick_params(axis='both', length=2., labelsize=tick_size)
 
-# Decide the ticklabel position in the new x-axis,
-# then convert them to the position in the old x-axis
-newlabel = rates
-ax2.set_xticks(count_spikes)
-ax2.tick_params(length=2., labelsize=tick_size)
-ax2.xaxis.set_ticks_position('bottom')
-ax2.xaxis.set_label_position('bottom')
-ax2.spines['bottom'].set_position(('outward', 25))
-ax2.set_xlabel('Firing rate (Hz)', size=label_size)
-ax2.set_xlim(ax[1].get_xlim())
-ax2.set_xticklabels(newlabel, size=tick_size)
+    # set titles only for upper plots
+    if key == 'rate':
+        title = 'T = ' + str(t_stops[0]) + ' s, $N_s$ = '+ str(ns[0])
+        ax[0][key_idx].set_title(title, size=label_size)
+    elif key == 'time':
+        title = '$N_s$ = ' + str(ns[0]) + ', $\lambda$ = '+ str(rates[0]) + ' Hz'
+        ax[0][key_idx].set_title(title, size=label_size)
+    elif key == 'neurons':
+        title = 'T = ' + str(t_stops[0]) + ' s, $\lambda$ = '+ str(rates[0]) + ' Hz'
+        ax[0][key_idx].set_title(title, size=label_size)
+    else:
+        raise ValueError('key not valid')
 
-# Put legend position
-axbox = ax[0].get_position()
-legend = ax[0].legend(loc="best", numpoints=1,
-                      markerscale=0.9, prop={"size": label_size - 1},
-                      frameon=True, borderpad=0.5)
-legend.get_frame().set_edgecolor('black')
+    if key_idx == 0:
+        ax[0][key_idx].set_ylabel("compute time (min)", size=label_size)
+        ax[1][key_idx].set_ylabel('log (compute time)', size=label_size)
+        # Put legend position
+        axbox = ax[0][key_idx].get_position()
+        legend = ax[0][key_idx].legend(loc="best", numpoints=1,
+                                       markerscale=0.9, prop={"size": label_size - 1},
+                                       frameon=True, borderpad=0.5)
+        legend.get_frame().set_edgecolor('grey')
+
+    # Set second x-axis
+    ax2 = ax[1][key_idx].twiny()
+
+    # Decide the ticklabel position in the new x-axis,
+    # then convert them to the position in the old x-axis
+    if key == 'rate':
+        newlabel = rates
+        label_add_ax = 'Firing rate (Hz)'
+    elif key == 'time':
+        newlabel = t_stops
+        label_add_ax = 'Time (s)'
+    elif key == 'neurons':
+        newlabel = ns
+        label_add_ax = 'Neurons'
+    else:
+        raise ValueError('key not valid')
+
+    ax2.set_xticks(count_spikes)
+    ax2.tick_params(length=2., labelsize=tick_size)
+    ax2.xaxis.set_ticks_position('bottom')
+    ax2.xaxis.set_label_position('bottom')
+    ax2.spines['bottom'].set_position(('outward', 25))
+    ax2.set_xlabel(label_add_ax, size=label_size)
+    ax2.set_xlim(ax[1][key_idx].get_xlim())
+    ax2.set_xticklabels(newlabel, size=tick_size)
+
+    # make the lower share the same ylim
+    y1, y2 = ax[1][key_idx].get_ylim()
+    y1_lower_plot = np.append(y1_lower_plot, y1)
+    y2_lower_plot = np.append(y2_lower_plot, y2)
+
+ymin = np.max(y1_lower_plot)
+ymax = np.max(y2_lower_plot)
+for key_idx, key in enumerate(keys):
+    ax[1][key_idx].set_ylim(ymin, ymax)
 
 # Comment this if you want set manually the space between edges and graph
 # see above subplots_adjust
