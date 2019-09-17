320779687d58f9dded678d2044b52df55cf6f5db
stellalessandra <a.stella@fz-juelich.de>
2019-09-17 11:44:59 +0200

    reformatted code - added if name main

--- multiple_pattern_durations/code/generate_art_data.py
+++ multiple_pattern_durations/code/generate_art_data.py
@@ -12,11 +12,11 @@ import math
 
 
 def generate_stp(occ, xi, t_stop, delays, t_start=0 * pq.s):
-    '''
+    """
     Generate a spatio-temporal-pattern (STP). One pattern consists in a
     repeated sequence of spikes with fixed inter spikes intervals (delays).
     The starting time of the repetitions of the pattern are randomly generated.
-    '''
+    """
     # Generating all the first spikes of the repetitions
     s1 = np.arange(0, (t_stop - t_start - delays[-1]).simplified.magnitude,
                    (t_stop - t_start - delays[-1]).simplified.magnitude/occ)
@@ -40,78 +40,89 @@ def generate_stp(occ, xi, t_stop, delays, t_start=0 * pq.s):
     return stp
 
 
-# Load general parameters
-with open("configfile.yaml", 'r') as stream:
-    config = yaml.load(stream, Loader=Loader)
+if __name__ == '__main__':
 
-xi = config['xi']
-winlen =config['winlen']
-binsize = config['binsize'] * pq.ms
-rate = config['rate'] * pq.Hz
-t_stop = config['t_stop'] * pq.s
-n = config['n']
-alpha = config['alpha']
-spectra = config['spectra']
+    # Load general parameters
+    with open("configfile.yaml", 'r') as stream:
+        config = yaml.load(stream, Loader=Loader)
 
-lengths = list(
-    range(0,(winlen +1) * int(binsize.magnitude), xi * int(
-        binsize.magnitude))) * binsize.units
+    xi = config['xi']
+    winlen = config['winlen']
+    binsize = config['binsize'] * pq.ms
+    rate = config['rate'] * pq.Hz
+    t_stop = config['t_stop'] * pq.s
+    n = config['n']
+    alpha = config['alpha']
+    spectra = config['spectra']
 
-# Probability to have one repetion of the pattern assuming Poiss
-p = (rate.simplified.magnitude * binsize.simplified.magnitude) ** xi
-num_combination_patt = (math.factorial(n)/math.factorial(n-xi)) * (winlen)
-# Computing min_occ as 0.01 percentile of a binominal(n_bins, p)
-sgnf_occ_found = False
-occ = 2
-while not sgnf_occ_found:
-    num_window_positions = (t_stop-winlen*binsize).simplified.magnitude/binsize.simplified.magnitude
-    p_occ = binom.pmf(
-        occ, num_window_positions, p) * num_combination_patt
-    if p_occ < alpha:
-        sgnf_occ_found = True
-    else:
-        occ += 1
+    lengths = list(
+        range(0, (winlen + 1) * int(binsize.magnitude), xi * int(
+            binsize.magnitude))) * binsize.units
 
-# Computation analytical p-values
-theo_pvalues = []
-for l in lengths:
-    num_combination_patt = (math.factorial(n) / math.factorial(n - xi)) * (l)
-    theo_pvalues.append(binom.sf(occ-1,
-        (t_stop.simplified.magnitude-l.simplified.magnitude)/binsize.simplified.magnitude, p)*num_combination_patt)
+    # Probability to have one repetition of the pattern assuming Poisson
+    p = (rate.simplified.magnitude * binsize.simplified.magnitude) ** xi
+    num_combination_patt = (math.factorial(n)/math.factorial(n-xi)) * winlen
+    # Computing min_occ as 0.01 percentile of a binomial(n_bins, p)
+    sgnf_occ_found = False
+    occ = 2
+    while not sgnf_occ_found:
+        num_window_positions = (t_stop-winlen*binsize).simplified.magnitude/binsize.simplified.magnitude
+        p_occ = binom.pmf(
+            occ, num_window_positions, p) * num_combination_patt
+        if p_occ < alpha:
+            sgnf_occ_found = True
+        else:
+            occ += 1
 
-# Generating the data
-# Fixing the seed
-np.random.seed(0)
-# Generating Independent Background
-background_sts = stg._n_poisson(rate=rate, t_stop=t_stop, n=n)
-# Total number of patterns
-num_neu_patt = len(lengths)*xi
-sts = []
-stps = []
-# Raising errors if the number of neurons is smaller than number of patterns
-if num_neu_patt>n:
-    raise ValueError('Number of neurons involved in the patterns is larger than total number of patterns')
-for l_idx, l in enumerate(lengths):
-    lag_bins = int(l / (xi-1))
-    delays = [i*lag_bins*binsize.magnitude for i in range(1,xi)]*binsize.units
-    print(delays)
-    # Generating patterns
-    stp = generate_stp(occ=occ,xi=xi,t_stop=t_stop,delays=delays)
-    stps.extend(stp)
-    # Merging the background spiketrains to the patterns
-    for p_idx, p in enumerate(stp):
-        sts.append(stg._pool_two_spiketrains(
-            background_sts[l_idx*xi + p_idx], p))
-sts.extend(background_sts[num_neu_patt:])
+    # Computation analytical p-values
+    theo_pvalues = []
+    for l in lengths:
+        num_combination_patt = (math.factorial(n) / math.factorial(n - xi)) * (l)
+        theo_pvalues.append(binom.sf(occ-1,
+            (t_stop.simplified.magnitude-l.simplified.magnitude)/binsize.simplified.magnitude, p)*num_combination_patt)
 
-data_path = '../data'
-# Create path is not already existing
-path_temp = './'
-for folder in split_path(data_path):
-    path_temp = path_temp + '/' + folder
-    mkdirp(path_temp)
-# Storing data
-np.save(data_path + '/art_data.npy', {'patt':stps, 'data':sts, 'params':{
-    'xi':xi, 'occ':occ, 'rate':rate, 't_stop':t_stop,'n':n, 'lengths':lengths,
-    'binsize':binsize, 'alpha':alpha}})
+    # Generating the data
+    # Fixing the seed
+    np.random.seed(0)
+    # Generating Independent Background
+    background_sts = stg._n_poisson(rate=rate, t_stop=t_stop, n=n)
+    # Total number of patterns
+    num_neu_patt = len(lengths)*xi
+    sts = []
+    stps = []
+    # Raising errors if the number of neurons is smaller than number
+    # of patterns
+    if num_neu_patt > n:
+        raise ValueError('Number of neurons involved in the patterns is '
+                         'larger than total number of patterns')
+    for l_idx, l in enumerate(lengths):
+        lag_bins = int(l / (xi-1))
+        delays = [i*lag_bins*binsize.magnitude for i in range(1, xi)]*binsize.units
+        print(delays)
+        # Generating patterns
+        stp = generate_stp(occ=occ, xi=xi, t_stop=t_stop, delays=delays)
+        stps.extend(stp)
+        # Merging the background spiketrains to the patterns
+        for p_idx, p in enumerate(stp):
+            sts.append(stg._pool_two_spiketrains(
+                background_sts[l_idx*xi + p_idx], p))
+    sts.extend(background_sts[num_neu_patt:])
 
+    data_path = '../data'
+    # Create path is not already existing
+    path_temp = './'
+    for folder in split_path(data_path):
+        path_temp = path_temp + '/' + folder
+        mkdirp(path_temp)
+    # Storing data
+    np.save(data_path + '/art_data.npy',
+            {'patt': stps,
+             'data': sts,
+             'params': {'xi': xi,
+                        'occ': occ,
+                        'rate': rate,
+                        't_stop': t_stop,
+                        'n': n,
+                        'lengths': lengths,
+                        'binsize': binsize,
+                        'alpha': alpha}})
--- multiple_pattern_durations/code/spade_analysis.py
+++ multiple_pattern_durations/code/spade_analysis.py
@@ -4,68 +4,61 @@ import os
 import argparse
 from mpi4py import MPI  # for parallelized routines
 import yaml
+from utils import mkdirp, split_path
 
-# Function to create new folders
-def mkdirp(directory):
-    if not os.path.isdir(directory):
-        os.mkdir(directory)
+if __name__ == '__main__':
+    data_file = np.load('../data/art_data.npy', encoding='latin1').item()
 
-# Function to split path to single folders
-def split_path(path):
-    folders = []
-    while 1:
-        path, folder = os.path.split(path)
-        if folder != "":
-            folders.append(folder)
-        else:
-            if path != "":
-                folders.append(path)
-            break
-    folders.reverse()
-    return folders
+    data = data_file['data']
+    data_param = data_file['params']
 
-data_file = np.load('../data/art_data.npy', encoding='latin1').item()
+    parser = argparse.ArgumentParser(description='Compute spade on artificial data '
+                                                 'for the given winlen and spectrum parameters')
+    parser.add_argument('winlen', metavar='winlen', type=int,
+                       help='winlen parameter of the spade function')
+    parser.add_argument('spectrum', metavar='spectrum', type=str,
+                       help='spectrum parameter of the spade function')
+    args = parser.parse_args()
 
-data = data_file['data']
-data_param = data_file['params']
+    min_spikes = data_param['xi']
+    max_spikes = data_param['xi']
 
-parser = argparse.ArgumentParser(description='Compute spade on artificial data '
-                                             'for the given winlen and spectrum parameters')
-parser.add_argument('winlen', metavar='winlen', type=int,
-                   help='winlen parameter of the spade function')
-parser.add_argument('spectrum', metavar='spectrum', type=str,
-                   help='spectrum parameter of the spade function')
-args = parser.parse_args()
+    with open("configfile.yaml", 'r') as stream:
+        config = yaml.load(stream)
+    n_surr = config['n_surr']
+    binsize = data_param['binsize']
+    winlen = args.winlen
+    spectrum = args.spectrum
+    param = {'winlen': winlen,
+             'n_surr': n_surr,
+             'binsize': binsize,
+             'spectrum': spectrum,
+             'min_spikes': min_spikes,
+             'max_spikes': max_spikes}
+    # Check MPI parameters
+    comm = MPI.COMM_WORLD   # create MPI communicator
+    rank = comm.Get_rank()  # get rank of current MPI task
+    size = comm.Get_size() # Get the total number of MPI processes
+    print('Number of processes:{}'.format(size))
 
-min_spikes = data_param['xi']
-max_spikes = data_param['xi']
-with open("configfile.yaml", 'r') as stream:
-    config = yaml.load(stream)
-n_surr = config['n_surr']
-binsize = data_param['binsize']
-winlen = args.winlen
-spectrum = args.spectrum
-param = {'winlen':winlen, 'n_surr':n_surr, 'binsize':binsize,
-         'spectrum':spectrum, 'min_spikes':min_spikes, 'max_spikes':max_spikes}
-# Check MPI parameters
-comm = MPI.COMM_WORLD   # create MPI communicator
-rank = comm.Get_rank()  # get rank of current MPI task
-size = comm.Get_size() # Get the total number of MPI processes
-print('Number of processes:{}'.format(size))
+    # Compute spade res
+    print('Running spade')
+    spade_res = spade.spade(data,
+                            binsize=binsize,
+                            winlen=winlen,
+                            n_surr=n_surr,
+                            min_spikes=min_spikes,
+                            max_spikes=max_spikes,
+                            spectrum=spectrum,
+                            alpha=1,
+                            psr_param=None)
+    
+    # Storing data
+    res_path = '../results/{}/winlen{}'.format(spectrum, winlen)
+    # Create path if not already existing
+    path_temp = './'
+    for folder in split_path(res_path):
+        path_temp = path_temp + '/' + folder
+        mkdirp(path_temp)
 
-# Compute spade res
-print('Running spade')
-spade_res = spade.spade(data, binsize=binsize, winlen=winlen, n_surr=n_surr,
-                           min_spikes=min_spikes, max_spikes=max_spikes,
-                        spectrum=spectrum, alpha=1, psr_param=None)
-
-
-# Storing data
-res_path = '../results/{}/winlen{}'.format(spectrum, winlen)
-# Create path is not already existing
-path_temp = './'
-for folder in split_path(res_path):
-    path_temp = path_temp + '/' + folder
-    mkdirp(path_temp)
-
-np.save(res_path + '/art_data_results.npy',[spade_res,param])
+    np.save(res_path + '/art_data_results.npy', [spade_res, param])
